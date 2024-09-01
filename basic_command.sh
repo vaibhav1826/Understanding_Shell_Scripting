6472597153f1baec17d6204b490af392d20c129f
Vaibhav bhatt <148764896+vaibhav1826@users.noreply.github.com>
2024-09-01 15:22:09 +0530

    initial commit

---
+++ basic_command.sh
@@ -0,0 +1,27 @@
+# command that are used -->
+
+# mkdir <directory_name> --> this is use to make directory 
+
+# cd <file_name> --> this is used to change the file or directory
+
+# touch <file_name> --> this is used to make a file 
+
+# rm <file_name> --> this is used to remove  a file
+
+# mv <file_name> --> this is used to move a directory or a file 
+
+# ls --> list directory contents
+
+# pwd --> print the current working directory
+
+#rmdir <directory_name> --> used to remove the directory  
+
+#cp <file_name> --> use to copy or duplicate the files
+
+#cat <file_name> --> display or concatnation of files
+
+#grep "pattern" <file_name> --> search text in the files
+
+#chmod <file_name> --> to change file permission
+
+#chown <file_name> --> change file ownership
